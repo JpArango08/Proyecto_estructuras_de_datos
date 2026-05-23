139e15c8a4b7df18bc6f165711a1ffbb97af005b
Your Name <you@example.com>
2026-05-23 18:55:19 -0500

    Nuevos metodos

--- consultas.py
+++ consultas.py
@@ -1,24 +1,36 @@
+from arbol_json import GeneralTree
 from lista_docs import LinkedList
+from json_control import JsonController
 import json
+
 class Consultas:
+
     def __init__(self):
-        data = LinkedList()
-    
+        self.data = LinkedList()
+
     def eq(self):
         ...
+
     def ne(self):
-        ...  
+        ...
+
     def gt(self):
-        ... 
+        ...
+
     def gte(self):
         ...
+
     def lt(self):
         ...
+
     def lte(self):
         ...
 
     def load(self, nombre_archivo: str):
-        ...
+        JsonController.json_a_arbol(nombre_archivo, self.data)
+
+
+
     def find(self, buscar: dict):
         ...
     
--- json_control.py
+++ json_control.py
@@ -1,6 +1,24 @@
 from arbol_json import GeneralTree
+from lista_docs import LinkedList
+import json
 class JsonController:
-    def json_a_arbol(nombre_archivo: str):
-        ...
-    def arbol_a_json(arbol: GeneralTree):
+    def agregar_nodos(self, elemento, padre, arbol):
+        for clave, valor in elemento.items():
+
+            if isinstance(valor, dict):
+                arbol.insert(padre,{clave: None})
+                self.agregar_nodos(valor,clave,arbol)
+            else:
+                arbol.insert(padre,{clave: valor})
+
+    def json_a_arbol(self,nombre_archivo: str, data: LinkedList):
+        with open(nombre_archivo, "r", encoding="utf-8") as archivo:
+            json_info = json.load(archivo)
+
+        for elemento in json_info:
+            arbol = GeneralTree()
+            arbol.insert(None,f"doc_{data.size}")
+            self.agregar_nodos(elemento,arbol.root,arbol)
+            data.append(arbol)
+    def arbol_a_json(self, arbol: GeneralTree):
         ...
